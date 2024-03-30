ae5368424876c566b88a02222e6055ee4d1b8ac2
amitmaji01 <amitmaji810156@gmail.com>
2024-03-30 14:55:16 +0530

    template change

--- template.py
+++ template.py
@@ -1,7 +1,7 @@
 import os
 from pathlib import Path
 import logging
-
+#Setting up logging
 logging.basicConfig(level=logging.INFO, format='[%(asctime)s]: %(message)s:')
 
 
@@ -40,16 +40,16 @@ for filepath in list_of_files:
     filepath = Path(filepath)
     #here i separate the folder and file using the split function
     filedir, filename = os.path.split(filepath)
-    #check if file directory 
+    # Checks if the directory part is exist or not if not exixt then creating .Logs this action using the logging module.
     if filedir !="":
         os.makedirs(filedir, exist_ok=True)
         logging.info(f"Creating directory; {filedir} for the file: {filename}")
-
+    #again chech if the file is exists or not and the size of the file 0 or not if any of condition true then file will be created 
     if (not os.path.exists(filepath)) or (os.path.getsize(filepath) == 0):
         with open(filepath, "w") as f:
             pass
             logging.info(f"Creating empty file: {filepath}")
 
-
+    #otherwise file already exists 
     else:
         logging.info(f"{filename} is already exists")
